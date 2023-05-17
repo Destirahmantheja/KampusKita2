004e8857e2bb6638f1ec61bef0d46009c8ee6ff2
Destirahmantheja <destitheja43742@gmail.com>
2023-05-17 13:10:23 +0700

    retrofit

---
+++ app/src/androidTest/java/API/APIRequestData.java
@@ -0,0 +1,36 @@
+package API;
+
+import Model.ModelResponse;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+
+public interface APIRequestData {
+    @GET("retrieve.php")
+    Call<ModelResponse> ardRetrieve();
+
+    @FormUrlEncoded
+    @POST("create.php")
+    Call<ModelResponse> ardCreate(
+            @Field("nama") String nama,
+            @Field("kota") String kota,
+            @Field("alamat") String alamat
+    );
+
+    @FormUrlEncoded
+    @POST("update.php")
+    Call<ModelResponse> ardUpdate(
+            @Field("id") String id,
+            @Field("nama") String nama,
+            @Field("kota") String kota,
+            @Field("alamat") String alamat
+    );
+
+    @FormUrlEncoded
+    @POST("delete.php")
+    Call<ModelResponse> ardDelete(
+            @Field("id") String id
+    );
+}
---
+++ app/src/androidTest/java/Model/ModelResponse.java
@@ -0,0 +1,19 @@
+package Model;
+
+import java.util.List;
+public class ModelResponse {
+    private String kode, pesan;
+    private List<ModelKampus> data;
+
+    public String getKode() {
+        return kode;
+    }
+
+    public String getPesan() {
+        return pesan;
+    }
+
+    public List<ModelKampus> getData() {
+        return data;
+    }
+}
